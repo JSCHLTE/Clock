d602919d5c5e944e73da1e96da360caa9a7a031b
Jordan <phoaxy@gmail.com>
2021-05-27 19:20:34 -0400

    Update
    - Updated clock for video.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,11 +1,19 @@
 //Jordan Schulte
+const appendDay = document.querySelector('.appendDay');
+const appendDate = document.querySelector('.appendDate');
 const appendTime = document.querySelector('.appendTime');
 
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
 function clock(){
-    const newDate = new Date();
-    let hours = newDate.getHours();
-    let minutes = newDate.getMinutes();
-    let seconds = newDate.getSeconds();
+    const date = new Date();
+    const dayNumber = date.getDay();
+    let month = date.getMonth() + 1;
+    let dayDate = date.getDate();
+    let year = date.getFullYear();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
     let prime = 'AM';
 
     if(hours > 12){
@@ -14,12 +22,13 @@ function clock(){
     }
 
     hours = (hours > 9) ? hours : `0${hours}`;
-    minutes = (minutes > 9) ? minutes : `0${minutes}`; //Makes the clock double 0
+    minutes = (minutes > 9) ? minutes : `0${minutes}`;
     seconds = (seconds > 9) ? seconds : `0${seconds}`;
 
     const time = `${hours}:${minutes}:${seconds} ${prime}`;
-
     appendTime.innerText = time;
+    appendDay.innerText = days[dayNumber];
+    appendDate.innerText = `${month}/${dayDate}/${year}`;
 };
 clock();
 setInterval(clock, 1000);
